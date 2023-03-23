0bb489212e7de362f26bd6ae8c3a6dac4ab36f98
Dominika Dziasek <ddziasek45@gmail.com>
2023-03-23 13:51:59 +0100

    fix total products

--- src/components/Header/HeaderCartButton.js
+++ src/components/Header/HeaderCartButton.js
@@ -1,9 +1,16 @@
 import { Button } from "react-bootstrap";
 import svg from "../../cart.svg";
 import classes from "./HeaderCartButton.module.css";
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+
+  const sumValues = ctx.items.reduce((currNum, items) => {
+    return currNum + items.amount;
+  }, 0);
+
   return (
     <React.Fragment>
       <Button
@@ -14,7 +21,7 @@ const HeaderCartButton = (props) => {
         onClick={props.onClick}
       >
         <img src={svg} alt="cart" className={classes.logo}></img>
-        <p className={classes.number}>0</p>
+        <p className={classes.number}>{sumValues}</p>
       </Button>
     </React.Fragment>
   );
