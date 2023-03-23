0b283299b298bcd7d2eb76cdf9e4ce014a36c7e0
Dominika Dziasek <ddziasek45@gmail.com>
2023-03-23 13:52:39 +0100

    main folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
-import ModalTemplate from "./components/Modal/Modal";
+import Filter from "./components/Filters/Filter";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./components/store/CartProvider";
+
 function App() {
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+  };
   const handleShow = () => {
     setShow(true);
   };
+
   return (
-    <React.Fragment>
-      <ModalTemplate show={show} handleClose={handleClose} title="Your Order" />
+    <CartProvider>
+      <Cart show={show} handleClose={handleClose} />
       <Header onClick={handleShow} />
+      <Filter />
       <Products />
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
